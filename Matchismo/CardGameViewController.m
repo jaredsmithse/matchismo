2e90c1e68428466011177df3c7c123e20a8ce431
Jared Smith <jared@zenpayroll.com>
2014-04-24 01:02:42 -0700

    Add images, button that changes state from front of card to back on click.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -14,16 +14,18 @@
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
+- (IBAction)touchCardButton:(UIButton *)sender
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    } else {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"flip!" forState:UIControlStateNormal];
+    }
+    
 }
 
 @end
